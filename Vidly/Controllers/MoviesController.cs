0ce0711b06af54242e8559481ac3be9b9e0a4ae6
Frank <frank@4idps.com>
2018-03-15 10:49:07 +0800

    Finish 2-11 Action Parameters

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -20,5 +20,20 @@ namespace Vidly.Controllers
                         //return new EmptyResult();
                         return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
                 }
+
+                public ActionResult Edit(int id)
+                {
+                        return Content("id=" + id);
+                }
+
+                public ActionResult Index(int? pageIndex, string sortBy)
+                {
+                        if (!pageIndex.HasValue)
+                                pageIndex = 1;
+                        if (String.IsNullOrWhiteSpace(sortBy))
+                                sortBy = "Name";
+
+                        return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+                }
         }
 }
