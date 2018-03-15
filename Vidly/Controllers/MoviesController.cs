7752d5f1b428e18a31ccc19978a63c04f4dcffd8
Frank <frank@4idps.com>
2018-03-15 15:59:16 +0800

    Finish 2-13 Attribute Routing

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -26,19 +26,10 @@ namespace Vidly.Controllers
                         return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
                 }
 
-                public ActionResult Edit(int id)
+                [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+                public ActionResult ByReleaseYear(int year, int month)
                 {
-                        return Content("id=" + id);
-                }
-
-                public ActionResult Index(int? pageIndex, string sortBy)
-                {
-                        if (!pageIndex.HasValue)
-                                pageIndex = 1;
-                        if (String.IsNullOrWhiteSpace(sortBy))
-                                sortBy = "Name";
-
-                        return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+                        return Content(year + "/" + month);
                 }
         }
 }
