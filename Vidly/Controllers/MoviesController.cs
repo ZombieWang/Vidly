8c8c0f4e71a5cc46210a196baad9a1a1075bada1
Frank <frank@4idps.com>
2018-03-16 11:33:02 +0800

    Finish 2-20 Solution

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
@@ -10,15 +7,31 @@ namespace Vidly.Controllers
 {
         public class MoviesController : Controller
         {
-                // GET: Movies
+                public ViewResult Index()
+                {
+                        var movies = GetMovies();
+
+                        return View(movies);
+                }
+
+                private IEnumerable<Movie> GetMovies()
+                {
+                        return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+                }
+
+                // GET: Movies/Random
                 public ActionResult Random()
                 {
                         var movie = new Movie() { Name = "Shrek!" };
                         var customers = new List<Customer>
-                        {
-                                new Models.Customer { Name = "Customer 1" },
-                                new Models.Customer { Name = "Customer 2" }
-                        };
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
 
                         var viewModel = new RandomMovieViewModel
                         {
@@ -27,6 +40,6 @@ namespace Vidly.Controllers
                         };
 
                         return View(viewModel);
-                } 
+                }
         }
 }
